53656aa28a9bf534c33c6fef2f160305902e1920
Kaj-Michael Lang <milang@tal.org>
2011-01-27 15:06:10 +0200

    Initial build system for dspeak

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x
+mkdir m4
+libtoolize --automake --copy --force
+aclocal-1.11 || aclocal
+autoconf --force
+autoheader --force
+gtkdocize --copy
+automake-1.11 --add-missing --copy --force-missing --foreign || automake --add-missing --copy --force-missing --foreign
